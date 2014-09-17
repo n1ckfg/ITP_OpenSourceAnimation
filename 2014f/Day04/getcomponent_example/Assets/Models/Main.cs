3fd6c95b40e8d7a89f214407967e7e84706f1b9b
Nick Fox-Gieg <nick@fox-gieg.com>
2014-09-16 21:30:33 -0400

    x

---
+++ 2014f/Day04/getcomponent_example/Assets/Models/Main.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	public GameObject target;
+	private Shake shake;
+
+	void Awake() {
+		shake = target.GetComponent<Shake>();
+	}
+
+	// Use this for initialization
+	void Start() {
+	
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		shake.shakeAmount += 0.01f;
+	}
+}
---
+++ 2014f/Day04/getcomponent_example/Assets/Models/Shake.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shake : MonoBehaviour {
+
+	public float shakeAmount = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 p = transform.position;
+		p.y += Random.Range(0f,shakeAmount) - Random.Range(0f,shakeAmount);
+		transform.position = p;
+	}
+}
