ac732fe77f32836c25557e153825e97372dd1880
Nick Fox-Gieg <nick@fox-gieg.com>
2014-09-30 19:39:16 -0400

    x

---
+++ 2014f/Day05/mecanim/Assets/TestScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A)) animator.SetTrigger("TriggerA");
+		if (Input.GetKeyDown(KeyCode.B)) animator.SetTrigger("TriggerB");
+	}
+}
