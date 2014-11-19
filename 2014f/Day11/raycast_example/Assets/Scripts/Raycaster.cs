42e24d5e030d8c7f95e0847bbb80d2e672c7429d
Nick Fox-Gieg <nick@fox-gieg.com>
2014-11-18 20:59:49 -0500

    x

---
+++ 2014f/Day11/raycast_example/Assets/Scripts/Raycaster.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Raycaster : MonoBehaviour {
+
+	public bool isLooking = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit)) {
+			Debug.Log(hit.collider.name);
+			if(hit.collider.name=="Cube") {
+				isLooking = true;
+			}
+		} else {
+			isLooking = false;
+		}
+
+	}
+
+}
