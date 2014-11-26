0130772a77f8382b7de6a41f4e1fa377cf7e108f
Nick Fox-Gieg <nick@fox-gieg.com>
2014-11-25 20:48:46 -0500

    x

---
+++ 2014f/Day12/collider_sound_example/Assets/Scripts/TriggerSound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerSound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	
+	}
+
+	void OnTriggerEnter() {
+		Debug.Log("triggered");
+		audio.Play();
+	}
+
+}
