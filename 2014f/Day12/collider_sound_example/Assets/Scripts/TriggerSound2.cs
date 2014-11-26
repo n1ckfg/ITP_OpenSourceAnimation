c7099d2bd2ca1a3e88b9b96c916227b3a987af55
Nick Fox-Gieg <nick@fox-gieg.com>
2014-11-25 20:57:23 -0500

    x

---
+++ 2014f/Day12/collider_sound_example/Assets/Scripts/TriggerSound2.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerSound2 : MonoBehaviour {
+
+	public GameObject target1;
+	public GameObject target2;
+
+	// Use this for initialization
+	void Start() {
+
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	
+	}
+
+	void OnTriggerEnter() {
+
+		float rnd = Random.Range(0f,1f);
+
+		Debug.Log(rnd);
+
+		if (rnd > 0.5f) {
+			target2.audio.Stop();
+			target1.audio.Play();
+		} else {
+			target1.audio.Stop();
+			target2.audio.Play();
+		}
+	}
+
+}
